4c1edf1dea1f9c66c3aa76b51e322207349dce6b
Inhere <in.798@qq.com>
2023-03-13 21:55:38 +0800

    :sparkles: feat(cli): add new command glab:resolve for quick resolve conflicts

--- internal/cli/gitcmd/glabcmd/resolve_conflict.go
+++ internal/cli/gitcmd/glabcmd/resolve_conflict.go
@@ -2,8 +2,9 @@ package glabcmd
 
 import (
 	"github.com/gookit/gcli/v3"
-	"github.com/gookit/goutil/errorx"
+	"github.com/inhere/kite/internal/app"
 	"github.com/inhere/kite/internal/biz/cmdbiz"
+	"github.com/inhere/kite/pkg/cmdutil"
 )
 
 var rcOpts cmdbiz.CommonOpts
@@ -26,7 +27,45 @@ var ResolveConflictCmd = &gcli.Command{
 		c.AddArg("branch", "The conflicts target branch name. eg: qa, pre, master", true)
 	},
 	Func: func(c *gcli.Command, args []string) error {
+		gl := app.Glab()
+		lr := gl.LoadRepo(rcOpts.Workdir)
 
-		return errorx.New("TODO")
+		br := c.Arg("branch").String()
+		br = lr.ResolveBranch(br)
+
+		rr := cmdutil.NewRunner(func(rr *cmdutil.Runner) {
+			rr.Workdir = rcOpts.Workdir
+			rr.DryRun = rcOpts.DryRun
+		})
+
+		rr.GitCmd("fetch", "-np")
+
+		if lr.HasLocalBranch(br) {
+			rr.GitCmd("checkout", br).
+				GitCmd("push", "-u", gl.DefaultRemote).
+				GitCmd("pull")
+		} else if lr.HasOriginBranch(br) {
+			// git checkout --track origin/NAME
+			rr.GitCmd("checkout", "--track", gl.OriginBranch(br)).
+				GitCmd("pull", gl.SourceRemote, br)
+		} else if lr.HasSourceBranch(br) {
+			rr.GitCmd("checkout", "--track", gl.SourceBranch(br)).
+				GitCmd("push", "-u", gl.DefaultRemote)
+		} else {
+			return c.NewErrf("branch %q not found in local and remotes", br)
+		}
+
+		curBr := lr.CurBranchName()
+		rr.GitCmd("pull", gl.SourceRemote, curBr)
+
+		if err := rr.Run(); err != nil {
+			return err
+		}
+
+		c.Println("---------------------------------------------------------")
+		c.Successln("Complete. please resolve conflicts by tools or manual...")
+		c.Warnln("TIP: Can execute follow command after resolved for quick commit:")
+		c.Infoln("  git add . && git commit && git push && kite gl pr -o head && git checkout", curBr)
+		return nil
 	},
 }
