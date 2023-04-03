732ce7e2f8a195fd9a195d054ede60e2f68bf29a
Inhere <in.798@qq.com>
2023-04-03 13:33:09 +0800

    :sparkles: feat(git): add new command tag:create for git commands

--- internal/cli/gitcmd/tag.go
+++ internal/cli/gitcmd/tag.go
@@ -3,7 +3,13 @@ package gitcmd
 import (
 	"errors"
 
+	"github.com/gookit/color/colorp"
 	"github.com/gookit/gcli/v3"
+	"github.com/gookit/gcli/v3/gflag"
+	"github.com/gookit/gcli/v3/interact"
+	"github.com/gookit/gcli/v3/show"
+	"github.com/gookit/gitw"
+	"github.com/gookit/gitw/gitutil"
 )
 
 // TagCmd instance
@@ -27,13 +33,80 @@ var TagListCmd = &gcli.Command{
 	},
 }
 
+var tcOpts = struct {
+	Next    bool   `flag:"create next version tag;false;n"`
+	Hash    string `flag:"create tag by commit hash;false;cid"`
+	Message string `flag:"tag message;false;m"`
+	Version string `flag:"tag version, eg: v2.0.1;false;v"`
+}{}
+
 // TagCreateCmd instance
 var TagCreateCmd = &gcli.Command{
 	Name:    "create",
 	Aliases: []string{"new"},
 	Desc:    "create new tag by `git tag`",
+	Config: func(c *gcli.Command) {
+		c.MustFromStruct(&tcOpts, gflag.TagRuleSimple)
+	},
 	Func: func(c *gcli.Command, args []string) error {
-		return errors.New("TODO")
+		lp := gitw.NewRepo(GitOpts.Workdir).
+			PrintCmdOnExec().
+			SetDryRun(GitOpts.DryRun)
+
+		err := lp.Cmd("pull", "-np").Run()
+		if err != nil {
+			return err
+		}
+
+		// git fetch --tags
+		err = lp.Cmd("fetch", "--tags").Run()
+		if err != nil {
+			return err
+		}
+
+		lastVer := lp.LargestTag()
+		nextVer := tcOpts.Version
+		if len(nextVer) == 0 {
+			nextVer = gitutil.NextVersion(lastVer)
+		} else {
+			var ok bool
+			nextVer, ok = gitutil.FormatVersion(nextVer)
+			if !ok {
+				return c.NewErrf("invalid version: %s", nextVer)
+			}
+		}
+
+		nextVer = "v" + nextVer
+		message := tcOpts.Message
+		if len(message) == 0 {
+			message = ":bookmark: release the " + nextVer
+		}
+
+		show.AList("create tag", map[string]any{
+			"Hash ID":     tcOpts.Hash,
+			"Prev tag":    lastVer,
+			"New tag":     nextVer,
+			"Tag Message": message,
+			"Dry Run":     GitOpts.DryRun,
+		})
+
+		if interact.Unconfirmed("ensure create and push tag?") {
+			colorp.Infoln("Quit create tag")
+			return nil
+		}
+
+		err = lp.Cmd("tag", "-a", nextVer, "-m", message).Run()
+		if err != nil {
+			return err
+		}
+
+		err = lp.Cmd("push", "origin", nextVer).Run()
+		if err != nil {
+			return err
+		}
+
+		colorp.Successf("Successful create tag: %s", nextVer)
+		return nil
 	},
 }
 
