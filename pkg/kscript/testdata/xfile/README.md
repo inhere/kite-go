4e35957874a00e735b611f4ddf6f33903eba5bbc
inhere <in.798@qq.com>
2025-07-23 22:41:33 +0800

    :necktie: up: kscript.xfile 添加定义和部分demo示例配置参考

---
+++ pkg/kscript/testdata/xfile/README.md
@@ -0,0 +1,16 @@
+# README
+
+xfile - 每个xfile文件都可以配置为一个类似makefile的工具文件，给一个项目使用。
+参考了 makefile, taskfile 的原理。 可以配置多个命令，用于执行自定义命令。
+
+使用：`kite xfile COMMAND`
+
+## 配置格式
+
+### mode: top_cfg
+
+
+
+### mode: setting
+
+
--- pkg/kscript/xfile.go
+++ pkg/kscript/xfile.go
@@ -1,8 +1,8 @@
 package kscript
 
-type XFile struct {
+type XFileManager struct {
 	// 离工作目录最近的一个 kitefile 文件
-	kf *KiteFile
+	top *XFile
 
 	// 允许的文件名列表 default: 'kitefile', '.kitefile', 'kitefile.yml', 'kitefile.yaml'
 	Filenames []string `json:"filenames"`
@@ -13,22 +13,49 @@ type XFile struct {
 	MaxFindDepth int `json:"max_find_depth"`
 }
 
-func NewXFile() *XFile {
-	return &XFile{
+func NewXFile() *XFileManager {
+	return &XFileManager{
 		Filenames: []string{"kitefile", ".kitefile"},
-		FileExts: DefaultDefineExts,
+		FileExts:  DefaultDefineExts,
 		MaxFindDepth: 5,
 	}
 }
 
-type KiteFile struct {
-	parent *KiteFile
+// XAction struct
+type XAction struct {
+	Name    string   `json:"name"`
+	Desc    string   `json:"desc"`
+	User    string   `json:"user"`
+	Workdir string   `json:"workdir"`
+	Cmds    []string `json:"cmds"`
+}
+
+type XFile struct {
+	parent *XFile
 	// 当前 .kitefile 文件路径
 	filePath string
+	// mode: top_cfg, setting
+	Mode string `json:"__xfile_mode"`
 
-	Name string // 脚本名称，help 时显示
-	Desc string // 描述 help 时显示
-
+	// 脚本名称，help 时显示
+	Name string `json:"name"`
+	// 描述信息 help 时显示
+	Desc    string `json:"desc"`
+	Version string `json:"version"`
+	Author  string `json:"author"`
+	// Homepage message
+	Homepage string `json:"homepage"`
 	// 包含/引用的公共定义文件
 	Include []string
+	// Env setting for run
+	Env map[string]string
+	// 运行时上下文
+	Context struct {
+		Workdir string `json:"workdir"`
+	} `json:"context"`
+	// DefaultAction on run
+	DefaultAction string `json:"default_action"`
+
+	// Actions 定义
+	Actions map[string]*XAction `json:"actions"`
 }
