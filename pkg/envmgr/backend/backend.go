5cc3731ed9f2a296ad02297a65d5498f902206f1
inhere <in.798@qq.com>
2025-10-13 10:51:06 +0800

    :necktie: up: update some for local env manager

---
+++ pkg/envmgr/backend/backend.go
@@ -0,0 +1,3 @@
+package backend
+
+// 安装工具，软件的后端: scoop, chocolatey, winget, http 等
---
+++ pkg/envmgr/shellenv/generator.go
@@ -0,0 +1,6 @@
+package shellenv
+
+
+// DefaultShellGenerator 默认Shell脚本生成器实现
+type DefaultShellGenerator struct {
+}
