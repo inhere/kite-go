44440a2debaa69e799556388fafcd6f014bcf22e
inhere <in.798@qq.com>
2025-11-20 17:26:11 +0800

    :necktie: up: 更新 RandomStrCmd 支持同时生成多个

--- internal/cli/textcmd/random_cmd.go
+++ internal/cli/textcmd/random_cmd.go
@@ -10,36 +10,44 @@ import (
 
 // NewRandomStrCmd create command
 func NewRandomStrCmd() *gcli.Command {
+	var randStrOpt = struct {
+		number  int
+		randTyp string
+	}{}
+
 	return &gcli.Command{
 		Name:    "random",
 		Aliases: []string{"rand"},
 		Desc:    "quick generate random string or number",
 		Config: func(c *gcli.Command) {
 			// random string(number,alpha,), int(range)
-			c.AddArg("length", "The length of random string. allow: 1-1024")
-			c.AddArg("type", `The type of random string.
+			c.StrOpt(&randStrOpt.randTyp, "type", "t", "an", `The type of random string.
 allow: num/number, a/alpha, hex, b64/base64, uuid, an/alpha_num`)
+			c.IntOpt(&randStrOpt.number, "number", "n", 1, "The number of random string")
+
+			c.AddArg("length", "The length of random string. allow: 1-1024").WithDefault("16")
 		},
 		Func: func(c *gcli.Command, _ []string) error {
-			n := c.Arg("length").Int()
-
 			var str string
-			switch c.Arg("type").String() {
-			case "num", "number":
-				fmt.Println("TODO")
-			case "a", "alpha":
-				str = strutil.RandWithTpl(n, strutil.AlphaBet)
-			case "hex":
-				str = strutil.RandWithTpl(n, strutil.HexChars)
-			case "b64", "base64":
-				str = strutil.RandWithTpl(n, strutil.Base64Chars)
-			case "an", "alpha_num":
-				str = strutil.RandWithTpl(n, strutil.AlphaNum2)
-			default:
-				return errorx.E("invalid type")
-			}
+			ln := c.Arg("length").Int()
 
-			fmt.Println(str)
+			for i := 0; i < randStrOpt.number; i++ {
+				switch randStrOpt.randTyp {
+				case "num", "number":
+					fmt.Println("TODO")
+				case "a", "alpha":
+					str = strutil.RandWithTpl(ln, strutil.AlphaBet)
+				case "hex":
+					str = strutil.RandWithTpl(ln, strutil.HexChars)
+				case "b64", "base64":
+					str = strutil.RandWithTpl(ln, strutil.Base64Chars)
+				case "an", "alpha_num":
+					str = strutil.RandWithTpl(ln, strutil.AlphaNum2)
+				default:
+					return errorx.Ef("invalid type: %s", randStrOpt.randTyp)
+				}
+				fmt.Println(str)
+			}
 			return nil
 		},
 	}
